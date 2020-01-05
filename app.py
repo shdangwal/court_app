f109a3496e3d1497ca1951b4609c0160d362cdee
Shubham Dangwal <shdangwal@gmail.com>
2020-01-05 17:07:06 +0530

    Fixed not importing db.

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from marshmallow import ValidationError
 
-from db import db
 from ma import ma
 from blacklist import BLACKLIST
 from resources.transaction import (
@@ -37,11 +36,6 @@ app.secret_key = "jose"  # could do app.config['JWT_SECRET_KEY'] if we prefer
 api = Api(app)
 
 
-@app.before_first_request
-def create_tabeles():
-    db.create_all()
-
-
 @app.errorhandler(ValidationError)
 def handle_marshmallow_validation(err):
     return jsonify(err.messages), 400
@@ -67,6 +61,5 @@ api.add_resource(TokenRefresh, "/refresh")
 api.add_resource(UserLogout, "/logout")
 
 if __name__ == '__main__':
-    db.init_app(app)
     ma.init_app(app)
     app.run(port=5000, debug=True)
